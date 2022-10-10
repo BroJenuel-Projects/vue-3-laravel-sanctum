75d65d1a11106c0366a785276b2ab6f5067aa23c
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-10-10 10:14:20 +0800

    Update SideMenus.vue

--- FrontEndApp/src/components/SideMenus/SideMenus.vue
+++ FrontEndApp/src/components/SideMenus/SideMenus.vue
@@ -1,32 +1,33 @@
-<template>
-    <NMenu :collapsed-width="64" :collapsed-icon-size="22" :value="menuValue" :options="menuOptions" :on-update:value="changeMenu" />
-</template>
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
-import { NMenu } from 'naive-ui';
-import { useRoute, useRouter } from 'vue-router';
-import { menuOptions } from './menuOptions';
-export default defineComponent({
-    components: {
-        NMenu,
-    },
-    setup() {
-        const router = useRouter();
-        const route = useRoute();
-        const menuValue = ref('');
-        const changeMenu = (key: string) => (menuValue.value = key);
-        onMounted(() => {
-            changeMenu(route.path);
-        });
+<script lang="ts" setup>
+import { ref, onMounted, watch } from "vue";
+import { NMenu } from "naive-ui";
+import { useRoute, useRouter } from "vue-router";
+import { menuOptions } from "./menuOptions";
 
-        return {
-            menuValue,
-            menuOptions,
-            changeMenu: (key: string) => {
-                changeMenu(key);
-                router.push(key);
-            },
-        };
-    },
+const route = useRoute();
+const router = useRouter();
+const menuValue = ref("");
+function changeMenu(key: string) {
+    router.push(key);
+}
+
+watch(
+    () => route.path,
+    (path) => {
+        menuValue.value = path;
+    }
+);
+
+onMounted(() => {
+    changeMenu(route.path);
 });
 </script>
+<template>
+    <NMenu
+        :collapsed-width="64"
+        :collapsed-icon-size="22"
+        :value="menuValue"
+        :options="menuOptions"
+        :on-update:value="changeMenu"
+    />
+</template>
